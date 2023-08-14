6c47143acc44aaa3fa62a248506a7bf33eeebfd1
Igor Lyubimov <97502872+lyubimovIgor@users.noreply.github.com>
2023-08-14 16:53:20 +0300

    Add files via upload

---
+++ logistic/views.py
@@ -0,0 +1,25 @@
+from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filterset_fields = ['products']
+
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response('Test deploy')
